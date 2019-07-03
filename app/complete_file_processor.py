c68e5a5293f068173fe654d1f0858a0fa22f3463
AdamHawtin <adam.hawtin@ons.gov.uk>
2019-07-03 23:31:48 +0100

    Improve logging

--- app/complete_file_processor.py
+++ app/complete_file_processor.py
@@ -13,44 +13,53 @@ from app.sftp_utility import SftpUtility
 from config import Config
 
 logger = wrap_logger(logging.getLogger(__name__))
+
 QM_SUPPLIER = 'QM'
 PPD_SUPPLIER = 'PPD'
 
 
-def process_complete_file(file: Path):
-    # TODO Encrypt, create manifest, send to sftp, delete partial file
-    action_type, pack_code, batch_id, batch_quantity = file.name.split('.')
+def process_complete_file(print_file_path: Path):
+    action_type, pack_code, batch_id, batch_quantity = print_file_path.name.split('.')
     print_file_name = get_print_file_name(pack_code)
 
-    # Encypt
-    logger.info('Encrypting complete print file', file=file)
+    # Encrypt
+    logger.info('Encrypting print file', file=str(print_file_path))
     encrypted_print_file_path = Config.ENCRYPTED_FILES_DIRECTORY.joinpath(print_file_name)
-    encrypted_message = pgp_encrypt_message(file.read_text(), get_supplier_for_pack_code(pack_code))
+    encrypted_message = pgp_encrypt_message(print_file_path.read_text(), get_supplier_for_pack_code(pack_code))
     encrypted_print_file_path.write_text(encrypted_message)
-    logger.info('Encrypted complete print file', file=file, encrypted_print_file=encrypted_print_file_path)
+    logger.info('Encrypted print file', file=str(print_file_path), encrypted_print_file=str(encrypted_print_file_path))
 
     # Create manifest
-    logger.info('Generating manifest for encrypted print file', encrypted_print_file=encrypted_print_file_path)
+    logger.info('Generating manifest for encrypted print file', encrypted_print_file=str(encrypted_print_file_path))
     manifest_file_path = Config.MANIFEST_FILES_DIRECTORY.joinpath(f'{print_file_name}.manifest')
     generate_manifest_file(manifest_file_path, encrypted_print_file_path, pack_code)
 
     # Send to SFTP
+    logger.info('Sending encrypted print file and manifest to SFTP',
+                encrypted_print_file=str(encrypted_print_file_path),
+                manifest_file_path=str(manifest_file_path))
     copy_files_to_sftp((manifest_file_path, encrypted_print_file_path), pack_code)
 
-    # TODO Delete
+    # Delete files
+    # TODO Shred files?
+    logger.info('Cleaning up local files',
+                files_to_delete=[
+                    str(file_path) for file_path in (print_file_path, encrypted_print_file_path, manifest_file_path)])
+    print_file_path.unlink()
+    encrypted_print_file_path.unlink()
+    manifest_file_path.unlink()
 
 
-def check_files(partial_files_dir: Path, sent_files_dir: Path):
-    for print_file in partial_files_dir.rglob('*'):
-        file_name_parts = print_file.name.split('.')
+def check_files(partial_files_dir: Path):
+    for print_file_path in partial_files_dir.rglob('*'):
+        file_name_parts = print_file_path.name.split('.')
         expected_number_of_lines = int(file_name_parts[3])
-        actual_number_of_lines = sum(1 for _ in print_file.open())
+        actual_number_of_lines = sum(1 for _ in print_file_path.open())
 
         if expected_number_of_lines == actual_number_of_lines:
-            process_complete_file(print_file)
-            print_file.replace(sent_files_dir.joinpath(print_file.name))
-            logger.info(f"File hero has encrypted the file [{print_file}],"
-                        f" created a manifest and SFTP'ed the two files")
+            logger.info('Processing complete print file', print_file_path=str(print_file_path))
+            process_complete_file(print_file_path)
+            logger.info('Finished processing print file', print_file_path=str(print_file_path))
 
 
 def start_file_sender(readiness_queue):
@@ -62,7 +71,7 @@ def start_file_sender(readiness_queue):
     readiness_queue.put(True)
     logger.info('Started file sender')
     while True:
-        check_files(Config.PARTIAL_FILES_DIRECTORY, Config.SENT_FILES_DIRECTORY)
+        check_files(Config.PARTIAL_FILES_DIRECTORY)
         sleep(2)
 
 
@@ -94,7 +103,8 @@ def get_sftp_directory(pack_code):
 
 def copy_files_to_sftp(file_paths: Collection[Path], pack_code):
     with sftp_utility.SftpUtility(get_sftp_directory(pack_code)) as sftp_client:
-        print(f'Copying files to SFTP remote {sftp_client.sftp_directory}')
+        logger.info('Copying files to SFTP remote directory', remote_directory=sftp_client.sftp_directory)
         for file_path in file_paths:
             sftp_client.put_file(local_path=str(file_path), filename=file_path.name)
-        print(f'All {len(file_paths)} files successfully written to {sftp_client.sftp_directory}')
+    logger.info('All files successfully written to SFTP', remote_directory=sftp_client.sftp_directory,
+                files=[str(file_path) for file_path in file_paths])
