514192fb307a9f5f0069df01061104295da0ae94
AdamHawtin <adam.hawtin@ons.gov.uk>
2019-08-13 11:36:54 +0100

    Fix rebase mistake, only use P_OR_HX actiontype for PQ fulfilments

--- app/mappings.py
+++ app/mappings.py
@@ -11,7 +11,7 @@ PACK_CODE_TO_DESCRIPTION = {
     PackCode.P_OR_H1: 'Household Questionnaire for England',
     PackCode.P_OR_H2: 'Household Questionnaire for Wales (English)',
     PackCode.P_OR_H2W: 'Household Questionnaire for Wales (Welsh)',
-    PackCode.P_OR_H4: 'Household Questionnaire for Northern Ireland (English)'
+    PackCode.P_OR_H4: 'Household Questionnaire for Northern Ireland (English)',
 }
 
 PACK_CODE_TO_DATASET = {
@@ -24,23 +24,23 @@ PACK_CODE_TO_DATASET = {
     PackCode.P_OR_H1: Dataset.QM3_4,
     PackCode.P_OR_H2: Dataset.QM3_4,
     PackCode.P_OR_H2W: Dataset.QM3_4,
-    PackCode.P_OR_H4: Dataset.QM3_4
+    PackCode.P_OR_H4: Dataset.QM3_4,
 }
 
 DATASET_TO_SUPPLIER = {
     Dataset.QM3_2: Supplier.QM,
     Dataset.QM3_4: Supplier.QM,
-    Dataset.PPD1_1: Supplier.PPO
+    Dataset.PPD1_1: Supplier.PPO,
 }
 
 SUPPLIER_TO_SFTP_DIRECTORY = {
     Supplier.QM: Config.SFTP_QM_DIRECTORY,
-    Supplier.PPO: Config.SFTP_PPO_DIRECTORY
+    Supplier.PPO: Config.SFTP_PPO_DIRECTORY,
 }
 
 SUPPLIER_TO_KEY_PATH = {
     Supplier.QM: Config.QM_SUPPLIER_PUBLIC_KEY_PATH,
-    Supplier.PPO: Config.PPO_SUPPLIER_PUBLIC_KEY_PATH
+    Supplier.PPO: Config.PPO_SUPPLIER_PUBLIC_KEY_PATH,
 }
 
 ACTION_TYPE_TO_PRINT_TEMPLATE = {
@@ -50,13 +50,10 @@ ACTION_TYPE_TO_PRINT_TEMPLATE = {
     ActionType.ICHHQE: PrintTemplate.QM_QUESTIONNAIRE_TEMPLATE,
     ActionType.ICHHQW: PrintTemplate.QM_QUESTIONNAIRE_TEMPLATE,
     ActionType.ICHHQN: PrintTemplate.QM_QUESTIONNAIRE_TEMPLATE,
-    ActionType.P_OR_H1: PrintTemplate.QM_QUESTIONNAIRE_TEMPLATE,
-    ActionType.P_OR_H2: PrintTemplate.QM_QUESTIONNAIRE_TEMPLATE,
-    ActionType.P_OR_H2W: PrintTemplate.QM_QUESTIONNAIRE_TEMPLATE,
-    ActionType.P_OR_H4: PrintTemplate.QM_QUESTIONNAIRE_TEMPLATE,
+    ActionType.P_OR_HX: PrintTemplate.QM_QUESTIONNAIRE_TEMPLATE,
 }
 
 SUPPLIER_TO_PRINT_TEMPLATE = {
     Supplier.PPO: PrintTemplate.PPO_LETTER_TEMPLATE,
-    Supplier.QM: PrintTemplate.QM_QUESTIONNAIRE_TEMPLATE
+    Supplier.QM: PrintTemplate.QM_QUESTIONNAIRE_TEMPLATE,
 }
