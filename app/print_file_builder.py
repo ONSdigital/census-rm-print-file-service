343f0a161a50eb4e915e91e9cd07dea32f0114b1
Luke Loze <Luke.Loze@ext.ons.gov.uk>
2019-09-03 12:17:18 +0100

    remove blank line

--- app/print_file_builder.py
+++ app/print_file_builder.py
@@ -36,7 +36,6 @@ def append_print_row(print_message, template, partial_print_file: Path):
 
 
 def get_message_metadata(print_message):
-
     try:
         action_type = ActionType(print_message['actionType'])
         pack_code = PackCode(print_message['packCode'])
