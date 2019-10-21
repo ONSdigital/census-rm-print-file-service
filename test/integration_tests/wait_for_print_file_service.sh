9915e362647e52c7df10deb23de2f9e47b52bdd6
AdamHawtin <adam.hawtin@ons.gov.uk>
2019-10-21 15:41:25 +0100

    Add max failed attempts to integration test startup

--- test/integration_tests/wait_for_print_file_service.sh
+++ test/integration_tests/wait_for_print_file_service.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 echo "Waiting for [print-file-service] to be ready"
+max_attempts=30
+attempt=0
 while true; do
     response=$(docker inspect dev-print-file-service -f "{{ .State.Health.Status }}")
     if [[ "$response" == "healthy" ]]; then
@@ -8,7 +10,13 @@ while true; do
     fi
 
     echo "[print-file-service] not ready ([$response] is its current state)"
+    attempt=$(attempt + 1)
+    if ((attempt > max_attempts )); then
+      echo "[print-file-service] failed to start"
+      exit 1
+    fi
     sleep 2s
+
 done
 
 echo "Containers running and alive"
