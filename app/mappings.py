b847d2e61c63fac503c97ae0450726b9a797e671
LeoKHoward <leo-howard@hotmail.co.uk>
2020-04-22 14:34:26 +0100

    Mappings

--- app/mappings.py
+++ app/mappings.py
@@ -188,7 +188,9 @@ ACTION_TYPE_TO_PRINT_TEMPLATE = {
     ActionType.P_RD_2RL2B_3: PrintTemplate.PPO_LETTER_TEMPLATE,
     ActionType.CE1_IC01: PrintTemplate.PPO_LETTER_TEMPLATE,
     ActionType.CE1_IC02: PrintTemplate.PPO_LETTER_TEMPLATE,
+    ActionType.CE_IC03: PrintTemplate.PPO_LETTER_TEMPLATE,
     ActionType.CE_IC03_1: PrintTemplate.PPO_LETTER_TEMPLATE,
+    ActionType.CE_IC04: PrintTemplate.PPO_LETTER_TEMPLATE,
     ActionType.CE_IC04_1: PrintTemplate.PPO_LETTER_TEMPLATE
 }
 
