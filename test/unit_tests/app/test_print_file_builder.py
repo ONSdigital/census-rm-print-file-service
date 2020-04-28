47b00772f8fe28cd2e1ed08c016b409cc117ac82
liamtoozer <liam.toozer@gmail.com>
2020-04-28 08:33:04 +0100

    Add tests

--- test/unit_tests/app/test_print_file_builder.py
+++ test/unit_tests/app/test_print_file_builder.py
@@ -50,7 +50,9 @@ def test_generate_print_row_valid_ICHHQE(cleanup_test_files):
         "addressLine2": "Duffryn",
         "townName": "Newport",
         "postcode": "NPXXXX",
-        "packCode": "P_IC_H1"
+        "packCode": "P_IC_H1",
+        "organisationName": "ONS",
+        "fieldOfficerId": "012345678"
     })
 
     # When
@@ -60,7 +62,7 @@ def test_generate_print_row_valid_ICHHQE(cleanup_test_files):
     generated_print_file = cleanup_test_files.partial_files.joinpath('ICHHQE.P_IC_H1.1.3')
     assert generated_print_file.read_text() == (
         'test_uac|test_qid|test_wales_uac|test_wales_qid|test_qm_coordinator_id|'
-        '|||123 Fake Street|Duffryn||Newport|NPXXXX|P_IC_H1\n')
+        '|||123 Fake Street|Duffryn||Newport|NPXXXX|P_IC_H1|ONS|012345678\n')
 
 
 def test_generate_print_row_valid_CE1_IC01(cleanup_test_files):
@@ -119,6 +121,66 @@ def test_generate_print_row_valid_CE_IC03_1(cleanup_test_files):
     assert generated_print_file.read_text() == ('test_uac|test_caseref||||123 Fake Street'
                                                 '|Duffryn||Newport|NPXXXX|D_ICA_ICLR1|21000000001234'
                                                 '|ONS|123456789|012345678\n')
+
+
+def test_generate_print_row_valid_CE_IC05(cleanup_test_files):
+    # Given
+    json_body = json.dumps({
+        "actionType": "CE_IC05",
+        "batchId": "1",
+        "batchQuantity": 3,
+        "uac": "test_uac",
+        "caseRef": "test_caseref",
+        "addressLine1": "123 Fake Street",
+        "addressLine2": "Duffryn",
+        "townName": "Newport",
+        "postcode": "NPXXXX",
+        "packCode": "D_CE4A_ICLR4",
+        "qid": "31000000000093",
+        "organisationName": "ONS",
+        "fieldCoordinatorId": "123456789",
+        "fieldOfficerId": "012345678"
+    })
+
+    # When
+    generate_print_row(json_body, cleanup_test_files.partial_files)
+
+    # Then
+    generated_print_file = cleanup_test_files.partial_files.joinpath('CE_IC05.D_CE4A_ICLR4.1.3')
+    assert generated_print_file.read_text() == ('test_uac|test_caseref||||123 Fake Street'
+                                                '|Duffryn||Newport|NPXXXX|D_CE4A_ICLR4|31000000000093'
+                                                '|ONS|123456789|012345678\n')
+
+
+def test_generate_print_row_valid_CE_IC08(cleanup_test_files):
+    # Given
+    json_body = json.dumps({
+        "actionType": "CE_IC08",
+        "batchId": "1",
+        "batchQuantity": 3,
+        "uac": "test_uac",
+        "qid": "test_qid",
+        "uacWales": "test_wales_uac",
+        "qidWales": "test_wales_qid",
+        "caseRef": "test_caseref",  # NB: ignored
+        "fieldCoordinatorId": "test_qm_coordinator_id",
+        "addressLine1": "123 Fake Street",
+        "addressLine2": "Duffryn",
+        "townName": "Newport",
+        "postcode": "NPXXXX",
+        "packCode": "D_FDCE_I4",
+        "organisationName": "ONS",
+        "fieldOfficerId": "012345678"
+    })
+
+    # When
+    generate_print_row(json_body, cleanup_test_files.partial_files)
+
+    # Then
+    generated_print_file = cleanup_test_files.partial_files.joinpath('CE_IC08.D_FDCE_I4.1.3')
+    assert generated_print_file.read_text() == (
+        'test_uac|test_qid|test_wales_uac|test_wales_qid|test_qm_coordinator_id||||123 Fake Street|Duffryn||Newport'
+        '|NPXXXX|D_FDCE_I4|ONS|012345678\n')
 
 
 def test_generate_print_row_invalid_action_type(cleanup_test_files):
