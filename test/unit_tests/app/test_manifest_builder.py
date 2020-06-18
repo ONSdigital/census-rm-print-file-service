d034cd2a6afacd7457dbea7a0432318a0f663314
AdamHawtin <adam.hawtin@ons.gov.uk>
2020-06-18 13:14:51 +0100

    Tiny refactor

--- test/unit_tests/app/test_manifest_builder.py
+++ test/unit_tests/app/test_manifest_builder.py
@@ -8,11 +8,9 @@ from app.manifest_file_builder import create_manifest
 from app.mappings import PACK_CODE_TO_DESCRIPTION, PACK_CODE_TO_DATASET
 from test.unit_tests import RESOURCE_FILE_PATH
 
-TEST_PACK_CODES = [pack_code for pack_code in PackCode]
-
 
 @patch('app.manifest_file_builder.datetime')
-@pytest.mark.parametrize("pack_code", TEST_PACK_CODES)
+@pytest.mark.parametrize("pack_code", [pack_code for pack_code in PackCode])
 def test_create_manifest(patch_datetime, pack_code):
     # Given
     dummy_print_file = RESOURCE_FILE_PATH.joinpath("dummy_print_file.txt")
