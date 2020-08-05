e15a79808bfc46022ffb046f99c299bf93a765ba
Apollo Akora <apollo.akora@ext.ons.gov.uk>
2020-08-05 02:08:27 +0100

    adding 3 pack codes to the print file service

--- app/constants.py
+++ app/constants.py
@@ -72,6 +72,9 @@ class ActionType(Enum):
     P_RL_1RL1B = 'P_RL_1RL1B'
     P_RL_1RL2BB = 'P_RL_1RL2BB'
     P_RL_1RL4A = 'P_RL_1RL4A'
+    P_RL_2RL4 = 'P_RL_2RL4'
+    P_RL_3RL1 = 'P_RL_3RL1'
+    P_RL_3RL2B = 'P_RL_3RL2B'
 
     # Reminder Questionnaires
     P_QU_H1 = 'P_QU_H1'
@@ -145,6 +148,10 @@ class PackCode(Enum):
     P_RL_1RL2B_2 = 'P_RL_1RL2B_2'
     P_RL_2RL1_3a = 'P_RL_2RL1_3a'
     P_RL_2RL2B_3a = 'P_RL_2RL2B_3a'
+
+    P_RL_2RL4 = 'P_RL_2RL4'
+    P_RL_3RL1 = 'P_RL_3RL1'
+    P_RL_3RL2B = 'P_RL_3RL2B'
 
     # Reminder letters for paper first households
     P_RL_1RL1B = 'P_RL_1RL1B'
